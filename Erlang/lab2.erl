4bae0a6295d3d987c3ad7e3f00c6a3dc379c9664
SzymonMajk <Szymek_diks@interia.pl>
2017-10-13 01:04:11 +0200

    fighting with maps and dicts

--- Erlang/lab2.erl
+++ Erlang/lab2.erl
@@ -1,10 +1,13 @@
 %% -*- coding: utf-8 -*-
 -module(lab2).
--export([map_append/3,map_update/3,map_display/1]).
+-export([map_append/3,map_update/3,map_display/1,countTokensFromFile/1]).
 -vsn(1.0).
--import(maps,[fold/3,to_list/1]).
+-import(maps,[fold/3,to_list/1,update_with/3]).
 -import(io,[fwrite/1]).
--import(lists,[map/2]).
+-import(lists,[map/2,foldl/3]).
+-import(unicode,[characters_to_list/1]).
+-import(string,[tokens/2]).
+-import(file,[read_file/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Zad 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -16,3 +19,31 @@ map_update(Key,Value,Map) -> Map#{Key := Value}.
 map_display(Map) -> print_elements(maps:to_list(Map)).
 print_elements([]) -> io:fwrite("Koniec",[]);
 print_elements([{K,V}]) -> io:fwrite("Key is: ~s,with value ~s~n",[K,V]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Zad 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+countTokensFromFile(Filename) -> 
+    countTokens(tokens(fileToString(read_file(Filename))," \n"),dict:new()).
+
+fileToString({_,FileBinaryContent}) -> 
+    unicode:characters_to_list(FileBinaryContent).
+
+%Almost...
+%countTokens([],Dict) -> Dict;
+%countTokens([Token|String],Dict) -> 
+%    countTokens(String,dict:update_counter(Token,1,Dict)).
+
+countTokens([],Dict) -> Dict;
+countTokens([Token|String],Dict) -> 
+    Fun = fun(V) -> V + 1 end,
+    countTokens(String,dict:update_counter(Token,1,Dict)).
+
+
+
+
+
+
+
+
+
+
