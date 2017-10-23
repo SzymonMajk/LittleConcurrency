40302b4ea542cd03a566e79bf1815dc75cc4327b
SzymonMajk <Szymek_diks@interia.pl>
2017-10-23 16:32:35 +0200

    final exercise with vector

--- AdaSekw2/pak3.ads
+++ AdaSekw2/pak3.ads
@@ -2,6 +2,7 @@ with Ada.Text_IO,Ada.Float_Text_IO,Ada.Numerics.Float_Random,Ada.Calendar;
 use Ada.Text_IO,Ada.Float_Text_IO,Ada.Numerics.Float_Random,Ada.Calendar;
 
 package pak3 is
+
 type vector is array(Integer range <>) of Float;
 procedure printVector(V : in vector);
 procedure randomVectorSet(V: in out vector);
