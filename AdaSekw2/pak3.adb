ce8560195483cc357f24c9210b0167c7b82e2821
SzymonMajk <Szymek_diks@interia.pl>
2017-10-23 16:32:35 +0200

    final exercise with vector

--- AdaSekw2/pak3.adb
+++ AdaSekw2/pak3.adb
@@ -1,16 +1,15 @@
-with Ada.Text_IO,Ada.Float_Text_IO,Ada.Numerics.Float_Random,Ada.Calendar;
-use Ada.Text_IO,Ada.Float_Text_IO,Ada.Numerics.Float_Random,Ada.Calendar;
+with Ada.Text_IO,Ada.Float_Text_IO,Ada.Numerics.Float_Random;
+use Ada.Text_IO,Ada.Float_Text_IO,Ada.Numerics.Float_Random;
 
 package body pak3 is
-
-
 
 procedure printVector(V : in vector) is
 begin
 	Put_Line("Start printing vector:");
 	for E of V loop
 		Put(E);
-		Put(", ");
+		--Put(", ");
+        New_Line;
 	end loop;
 	Put_Line("That is my vector!");
 end printVector;
@@ -38,21 +37,13 @@ J: Integer;
 begin
 	Put_Line("Sort the vector!");
     for I in V'First+1..V'Last loop
-    Key := V(I);
-    J := I - 1;
-        for K in reverse I-1..V'First loop
-            if Key < V(K) then
-                J := K;
-                exit;
-            else
-                V(K+1) := V(K);
-            end if;
+        Key := V(I);
+        J := I-1;
+        while V(J) > Key loop
+            V(J+1) := V(J);
+            J := J-1;
+            if (j < V'First) then exit; end if;            
         end loop;
---TODO !!!
-            --while J > V'First and Key < V(J) loop
-            --    V(J+1) := V(J);
-            --    J := J - 1;
-            --end loop;   
         V(J+1) := Key;
     end loop;
 end sortVector;
