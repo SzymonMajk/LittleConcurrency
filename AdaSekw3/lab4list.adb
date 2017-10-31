f4e2eaf5efe1bd8b0736b1721379abb4622bdf44
SzymonMajk <Szymek_diks@interia.pl>
2017-10-31 10:47:42 +0100

    ech, lists

--- AdaSekw3/lab4list.adb
+++ AdaSekw3/lab4list.adb
@@ -1,7 +1,5 @@
--- lab4.adb
--- Materiały dydaktyczne
--- J.P. 2017
--- compile with lab4list.adb -gnata -o lab4list.out
+-- Materiały dydaktyczne J.P. 2017
+-- to test compile gnatmake lab4list.adb -gnata -o lab4list.out
 
 with Ada.Text_IO, Ada.Integer_Text_IO;
 use Ada.Text_IO, Ada.Integer_Text_IO;
@@ -42,7 +40,7 @@ function insertFront(List : access Element; D : in Integer)
 	return access Element is ( new Element'(D,List) );  
    
 procedure insertWithSort(List : in out Elem_Ptr; D : in Integer) is
-Tmp, Last : Elem_Ptr := List;
+Tmp, Prev : Elem_Ptr := List;
 E : Elem_Ptr := new Element;
 begin
     E.all := (D,Null);
@@ -50,31 +48,46 @@ begin
 	if List = Null then
 		insertFront(List,D);
 	elsif List.Next = Null then
-        if(List.Data < D) then
+        if(List.Data > D) then 
             List.Next := E;
         else
-            insertFront(List,D);
+            insertFront(List,D); 
         end if;
+    elsif List.Data < D then
+        insertFront(List,D);
     else
-		while Tmp.Next /= Null loop
+        Tmp := Tmp.Next;
+		while Tmp /= Null loop
             if(List.Data < D) then
-                E.Next := Tmp.Next;
-                exit;
+                E.Next := Tmp;
+                Prev.Next := E;
+                return;
             else
+                Prev := Tmp;
 			    Tmp := Tmp.Next;
             end if;
 		end loop;
-		Tmp.Next := E;
+        Prev.Next := E;
 	end if;
 end insertWithSort;       
 
 List : Elem_Ptr := Null;
+List2 : Elem_Ptr := Null;
+
 
 begin 
-	print(List);
-	for I in reverse 1..6 loop
+	--print(List);
+	for I in 1..6 loop
 		insertWithSort(List, I);
+        --pragma Debug(print(List));
+        --insertWithSort(List, 4);
 		pragma Debug(print(List));
 	end loop;
-	print(List);
+
+	for I in reverse 1..6 loop
+		insertWithSort(List2, I);
+        --insertWithSort(List2, 7);
+		pragma Debug(print(List2));
+	end loop;
+	--print(List);
 end Lab4List;
