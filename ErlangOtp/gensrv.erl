e625824fc812f2c9afb89a3ee0e5edfcf31112de
Szymon Majkut <szymon.majkut@imac-06.local>
2017-12-12 13:59:31 +0100

    first stand with gen_server

---
+++ ErlangOtp/gensrv.erl
@@ -0,0 +1,149 @@
+%%%-------------------------------------------------------------------
+%%% @author ptm
+%%% @copyright (C) 2017, ptm
+%%% @doc
+%%%
+%%% @end
+%%% Created : 2017-11-30 12:21:47.705561
+%%%-------------------------------------------------------------------
+-module(gensrv).
+
+%% Api
+-export([start_link/0,add/1,history/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state,{dict,history}).
+%% TODO sproowac zmienic na mape
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+add(V) ->
+	gen_server:call(?SERVER,{add,V}).
+history() ->
+	gen_server:call(?SERVER,history).
+eraseHistory() ->
+	gen_server:call(?SERVER,{eraseHistory}).
+	
+%% gen_server generated
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, #state{dict=[],history=[]}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({add,V},From,State) ->
+	AddReply = ok,
+	{reply, AddReply, #state{
+	dict=[V|State#state.dict],
+	history=[{add,From,V}|State#state.history]}};
+handle_call(history,From,State) ->
+	%I dont cache history requests
+	Reply = State#state.history,
+	{reply, Reply, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast({eraseHistory}, State) ->
+    {noreply, #state{dict=State#state.dict,history=[]}}.
+%handle_cast(_Msg, State) ->
+    %{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+        {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
